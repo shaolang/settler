abbd0f945819044308725f5e52b0c97519cdba4f
shaolang <shaolang@ymail.com>
2020-02-19 09:05:20 +0800

    Default weekends as Sat/Sun and spot-lag as 2 when not set up
    ... 'cos bulk of the configs are as such, so defaulting them can help in
    keeping config lean.

--- src/settler/core.clj
+++ src/settler/core.clj
@@ -1,7 +1,12 @@
 (ns settler.core
-  (:require [clojure.set :as set]))
+  (:require [clojure.set :as set])
+  (:import [java.time DayOfWeek]))
 
-(defn to-next-biz-day [weekends date days-to-add]
+(defonce ^:private STANDARD-WEEKEND #{DayOfWeek/SATURDAY DayOfWeek/SUNDAY})
+(defonce ^:private STANDARD-SPOT-LAG 2)
+
+
+(defn- to-next-biz-day [weekends date days-to-add]
   (let [new-date (.plusDays date days-to-add)]
     (if (some #{(.getDayOfWeek new-date)} weekends)
       (recur weekends new-date 1)
@@ -10,9 +15,9 @@
 
 (defn spot [spot-lags configs trade-date ccy1 ccy2]
   (let [pair          #{ccy1 ccy2}
-        spot-lag      (get spot-lags pair)
-        ccy1-weekends (get-in configs [ccy1 :weekends])
-        ccy2-weekends (get-in configs [ccy2 :weekends])
+        spot-lag      (get spot-lags pair STANDARD-SPOT-LAG)
+        ccy1-weekends (get-in configs [ccy1 :weekends] STANDARD-WEEKEND)
+        ccy2-weekends (get-in configs [ccy2 :weekends] STANDARD-WEEKEND)
         spot1         (to-next-biz-day ccy1-weekends trade-date spot-lag)
         spot2         (to-next-biz-day ccy2-weekends trade-date spot-lag)
         candidate     (if (.isAfter spot1 spot2) spot1 spot2)]
--- test/settler/core_test.clj
+++ test/settler/core_test.clj
@@ -1,10 +1,12 @@
 (ns settler.core-test
   (:require [clojure.string :as str]
+            [clojure.test :refer [deftest is testing]]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :refer [for-all]]
             [settler.core :as settler])
-  (:import [java.time DayOfWeek LocalDate]))
+  (:import [java.time DayOfWeek LocalDate]
+           [java.time.temporal ChronoUnit]))
 
 ;;;;;;;;;;;;;
 ;; generators
@@ -34,3 +36,40 @@
           spot-date     (settler/spot spot-lag-map configs trade-date ccy1 ccy2)
           spot-day      (.getDayOfWeek spot-date)]
       (not (some #{spot-day} all-weekends)))))
+
+;;;;;;;;;;
+;; helpers
+
+(defn days-between [start end]
+  (.until start end ChronoUnit/DAYS))
+
+
+(defn rand-date []
+  (.plusDays (LocalDate/now) (rand-int 100)))
+
+;;;;;;;;
+;; tests
+
+(deftest spot-lag-determines-days-between-trade-and-spot-date
+  (doseq [spot-lag [0 1 2 3]]
+    (testing (str "spot-lag = " spot-lag)
+      (let [configs       (zipmap ["ABC" "DEF"] (repeat {:weekends #{}}))
+            spot-lag-map  {#{"ABC" "DEF"} spot-lag}
+            trade-date    (rand-date)
+            spot-date     (settler/spot spot-lag-map configs trade-date
+                                        "ABC" "DEF")]
+        (is (= spot-lag (days-between trade-date spot-date)))))))
+
+
+(deftest spot-lag-defaults-to-2-if-not-setup
+  (let [configs     (zipmap ["UVW" "XYZ"] (repeat {:weekends #{}}))
+        trade-date  (rand-date)
+        spot-date   (settler/spot {} configs trade-date "UVW" "XYZ")]
+    (is (= 2 (days-between trade-date spot-date)))))
+
+
+(deftest weekends-defaults-to-sats-and-suns-if-not-setup
+  (let [trade-date  (LocalDate/of 2015 1 1)   ;; Thursday
+        spot-date   (settler/spot nil nil trade-date "GHI" "JKL")
+        spot-day    (.getDayOfWeek spot-date)]
+    (is (= spot-date (LocalDate/of 2015 1 5)))))    ;; Tuesday
